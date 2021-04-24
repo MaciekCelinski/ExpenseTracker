8923ce29f5a5641f8b205e91b13fa7483acfd06a
Maciej Celinski <maciej.celinski@ives-system.com>
2021-04-24 22:01:02 +0200

    use context added to components, delete reducer and action added

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GobalState";
 
 function IncomeExpenses() {
+	const { transactions } = useContext(GlobalContext);
+
+	const incomeValues = transactions.filter(
+		(transaction) => transaction.amount > 0
+	);
+	const income = incomeValues.reduce((total, item) => total + item.amount, 0);
+
+	const expenseValues = transactions.filter(
+		(transaction) => transaction.amount < 0
+	);
+	const expense = Math.abs(
+		expenseValues.reduce((total, item) => total - item.amount, 0)
+	);
+
 	return (
 		<div className="inc-exp-container">
 			<div>
 				<h4>Income</h4>
 				<p id="money-plus" className="money plus">
-					+$0.00
+					${income}
 				</p>
 			</div>
 			<div>
 				<h4>Expense</h4>
 				<p id="money-minus" className="money minus">
-					+$0.00
+					${expense}
 				</p>
 			</div>
 		</div>
